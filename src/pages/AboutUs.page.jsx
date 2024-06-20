c44a7762d970a4af87b4f11e0782a60650aece12
mehdi-khallaghi <mkh.computer@gmail.com>
2024-06-20 13:05:22 +0330

    end of contact us

--- src/pages/AboutUs.page.jsx
+++ src/pages/AboutUs.page.jsx
@@ -1,13 +1,33 @@
 import React from 'react'
+import aboutImg from '../assets/images/about.png'
 
 const AboutUsPage = () => {
   return (
-    <div>
-      <h5>AboutUsPage</h5>
-      <h5>AboutUPage</h5>
-      <h5>AboutUsPage</h5>
+    <div className='container'>
+      <div className="row">
+        <div className="col-12 bg-info p-3 rounded mt-3">
+          <h3>about us</h3>
+        </div>
+      </div>
+      <div className="row align-items-center mt-3">
+        <div className="col-md-7 ">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Aut facilis vel cupiditate, est, possimus,
+          incidunt nisi atque esse dolorum perferendis eius! Cupiditate qui iste itaque,
+          expedita a eum reprehenderit alias?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Aut facilis vel cupiditate, est, possimus,
+          incidunt nisi atque esse dolorum perferendis eius! Cupiditate qui iste itaque,
+          expedita a eum reprehenderit alias?
+        
+        </div>
+        <div className="col-md-5 text-center">
+          <img src={aboutImg} alt="about" className='img-fluid about-img' />
+        </div>
+      </div>
     </div>
   )
 }
 
 export default AboutUsPage;
+ 
--- src/pages/ContactUs.page.jsx
+++ src/pages/ContactUs.page.jsx
@@ -1,12 +1,41 @@
 import React from 'react'
+import contactImg from '../assets/images/contact.png'
 
 const ContactUsPage = () => {
   return (
-    <div>
-      <h5>ContactUsPage</h5>
-      <h5>ContactUsPage</h5>
-      <h5>ContactUsPage</h5>
+    <div className='container'>
+      <div className="row">
+        <div className="col-12 bg-info p-3 rounded mt-2">
+          <h3>contact us</h3>
+        </div>
+      </div>
+      <div className="row align-items-center mt-3">
+        <div className="col-md-7">
+          <div>
+            <h3 className="text-primary">How to contact us</h3>
+            <h5>+98 0912 088 5499</h5>
+            <h5>+21 12 34 56 78</h5>
+            <h5>m.khcomputer@gmail.com</h5>
+          </div>
+          <div className='mt-3'>
+            <div className="mb-3">
+              <label class="form-label">Your Name : </label>
+              <input type="text" className="form-control" placeholder="Please type your name..." />
+            </div>
+            <div className="mb-3">
+              <label class="form-label">Your Message : </label>
+              <input type="text" className="form-control" placeholder="Please type your message..." />
+            </div>
+            <button className="btn btn-outline-primary ms-3">Send</button>
+          </div>
+        </div>
+
+      </div>
+      <div className="col-md-4 text-center">
+        <img src={contactImg} alt="about" className='img-fluid about-img' />
+      </div>
     </div>
+
   )
 }
 
